3c49b3555392e36fd21433ff6906b0b7a43789e7
Sam <106921485+uizyk@users.noreply.github.com>
2022-11-06 17:53:59 +0900

    finished functionality of advice generator

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.adviceslip.com/advice');
-xhr.send(null);
-xhr.addEventListener('readystatechange', function(){
-    if (xhr.readyState === XMLHttpRequest.DONE){
-        let response = JSON.parse(xhr.responseText);
-        console.log(xhr);
-    }
+let dice = document.getElementById("dice");
+let adviceNumber = document.getElementById("advice-number");
+let advice = document.getElementById("advice");
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+dice.addEventListener("click", function(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.adviceslip.com/advice/${getRandomInt(0, 224)}`);
+    xhr.send(null);
+    xhr.addEventListener('readystatechange', function(){
+        if (xhr.readyState === XMLHttpRequest.DONE){
+            let response = JSON.parse(xhr.responseText);
+            console.log(response.slip);
+            adviceNumber.textContent = `Advice #${response.slip.id}`;
+            advice.textContent = response.slip.advice;
+
+        }
+    })
 })
+
+
+
+
 
 // console.log(response);
